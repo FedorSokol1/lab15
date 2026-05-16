792875db4e8c92d691458375d8aeffc04b1ecc6e
FedorSokol1 <fedorik7@gmail.com>
2026-05-16 12:48:50 +0300

    Update section.jsx

--- src/section.jsx
+++ src/section.jsx
@@ -3,8 +3,11 @@ import picture from './Screenshot 2025-12-05 003108.png';
 function Section() {
   return (
     <section>
-      <img src={picture} alt="Картинка" />
-    </section>
+
+      <img
+        src={picture}
+        alt="Картинка"
+      />    </section>
   );
 }
 
